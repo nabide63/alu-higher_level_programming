cebc6b07956bddfb7d2c25b40d972bcfc052442d
Nabide Yusuf <y.nabide@alustudent.com>
2025-03-08 16:36:09 +0200

    corrected the no.11 script

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    maxv = float('-inf')
-    maxk = None
-    for k, v in a_dictionary.items():
-        if v > maxv:
-            maxk = k
-            maxv = v
-    return maxk
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
